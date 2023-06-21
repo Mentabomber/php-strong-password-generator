2ed14305f8e5358ab499064db350e35f1715a5ec
Menta <simcictilen.gmail.com>
2023-06-21 16:47:23 +0200

    prove funzione generatrice array psw

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <?php 
-    
+        error_reporting(E_ALL);
     ?>
 </head>
 <body>
@@ -21,7 +21,7 @@
 <?php 
 
     
-    rngPsw($length){
+    function rngPsw($length){
             $allLettersArray = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z","A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","0","1","2","3","4","5","6","7","8","9","'",'"',"£","$","%","&","/","(",")","=","?",":","^",",",".",];
 
             $charactersArray = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
@@ -33,9 +33,22 @@
             $symbolsArray =["'",'"',"£","$","%","&","/","(",")","=","?",":","^",",","."];
             
             $psw = [];
-            for ($i=0; $i <= $length; $i++) { 
+            for ($i=0; $i < $length; $i++) { 
+                
+                // $psw[] = array_search(rand(0, 25), $charactersArray);
+                // echo "<br>";
+                // var_dump($psw);
+                // echo "<br>";
+
+
                 $psw[] = array_search(rand(0, 76), $allLettersArray);
+                echo "<br>";
+                var_dump(rand(0, 76));
+                echo "<br>";
+                var_dump($psw);
+                echo "<br>";
             }
+            
         return $psw;
     };
 
