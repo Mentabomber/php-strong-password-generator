c9aa2f3350f713fe85778179b9b519b230635256
Menta <simcictilen.gmail.com>
2023-06-21 17:21:55 +0200

    milestone 2 include + file functions.php

--- functions.php
+++ functions.php
@@ -1,19 +1,36 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <?php 
-    include __DIR__ . 'functions.php';
-    ?>
-</head>
-<body>
-
-</body>
-</html>
-
 <?php 
+    
+    error_reporting(E_ALL);
 
+    $pswLength = $_GET["length"];
+    function rngPsw($length){
+            $allLettersArray = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z","A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","0","1","2","3","4","5","6","7","8","9","'",'"',"£","$","%","&","/","(",")","=","?",":","^",",",".",];
+
+            $charactersArray = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+
+            $capsCharactersArray = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+
+            $numbersArray = ["0","1","2","3","4","5","6","7","8","9"];
+
+            $symbolsArray =["'",'"',"£","$","%","&","/","(",")","=","?",":","^",",","."];
+            
+            $psw = [];
+            for ($i=0; $i < $length; $i++) { 
+                
+                // $psw[] = array_search(rand(0, 25), $charactersArray);
+                // echo "<br>";
+                // var_dump($psw);
+                // echo "<br>";
+
+
+                $psw[] = $allLettersArray[rand(0, 76)];
+                // echo "<br>";
+                // var_dump(rand(0, 76));
+                // echo "<br>";
+                // var_dump($psw);
+                // echo "<br>";
+            }
+            
+        return $psw;
+    };
 ?>
--- index.php
+++ index.php
@@ -6,42 +6,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <?php 
+        
         error_reporting(E_ALL);
-
-        $pswLength = $_GET["length"];
-        function rngPsw($length){
-                $allLettersArray = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z","A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","0","1","2","3","4","5","6","7","8","9","'",'"',"£","$","%","&","/","(",")","=","?",":","^",",",".",];
-    
-                $charactersArray = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
-    
-                $capsCharactersArray = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
-    
-                $numbersArray = ["0","1","2","3","4","5","6","7","8","9"];
-    
-                $symbolsArray =["'",'"',"£","$","%","&","/","(",")","=","?",":","^",",","."];
-                
-                $psw = [];
-                for ($i=0; $i < $length; $i++) { 
-                    
-                    // $psw[] = array_search(rand(0, 25), $charactersArray);
-                    // echo "<br>";
-                    // var_dump($psw);
-                    // echo "<br>";
-    
-    
-                    $psw[] = $allLettersArray[rand(0, 76)];
-                    // echo "<br>";
-                    // var_dump(rand(0, 76));
-                    // echo "<br>";
-                    // var_dump($psw);
-                    // echo "<br>";
-                }
-                
-            return $psw;
-        };
-    
-    
-   
         
     ?>
 </head>
@@ -53,6 +19,7 @@
 
         <span>La tua password è: 
             <?php  
+                include 'functions.php';
                 $generatedPsw =  rngPsw($pswLength);
                 foreach ($generatedPsw as $character) {
                     echo $character;
