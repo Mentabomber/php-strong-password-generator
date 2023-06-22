87373ed525dc6e958cb05e691a0838a6cf6abbe1
Menta <simcictilen.gmail.com>
2023-06-22 10:54:43 +0200

    modifiche after correzione

--- functions.php
+++ functions.php
@@ -3,17 +3,40 @@
     error_reporting(E_ALL);
 
     $pswLength = $_GET["length"];
+    // $repetitionChoice = $_GET[""];
+    $pswLettersChoice = $_GET["letters"];
+    $pswCharactersChoice = $_GET["characters"];
+    $pswSymbolsChoice = $_GET["symbols"];
+    $pswNumbersChoice = $_GET["numbers"];
+
+
     function rngPsw($length){
             $allLettersArray = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z","A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","0","1","2","3","4","5","6","7","8","9","'",'"',"£","$","%","&","/","(",")","=","?",":","^",",",".",];
-
+                                // = [...$charactersArray, ...$charactersArray, ...$numbersArray, ...$symbolsArray];
             $charactersArray = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
-
+                                // = range("a","z");
             $capsCharactersArray = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
-
+                                // = range("A","Z");
             $numbersArray = ["0","1","2","3","4","5","6","7","8","9"];
-
+                            // = range("0","9");
             $symbolsArray =["'",'"',"£","$","%","&","/","(",")","=","?",":","^",",","."];
-            
+                            // = [...range("!","/"),
+                            //    ...range(":","@"),
+                            //    ...range("[","`"),
+                            //    ...range("{","~") ];
+
+            if($pswLength === null || $pswLength === "") return;
+            if ($charactersArray === null && $numbersArray === null && $symbolsArray === null) return;
+
+            $selected = [];
+
+            // if($pswLettersChoice == "on"){
+            //     for ($i=0; $i < count($); $i++) { 
+                    
+            //     }
+            // }
+
+
             $psw = [];
             for ($i=0; $i < $length; $i++) { 
                 
@@ -22,8 +45,10 @@
                 // var_dump($psw);
                 // echo "<br>";
 
-
-                $psw[] = $allLettersArray[rand(0, 76)];
+                $randInt = rand(0, count($allLettersArray) - 1);
+                $psw[] = $allLettersArray[$randInt];
+                
+                // $psw[] = $allLettersArray[rand(0, 76)];
                 // echo "<br>";
                 // var_dump(rand(0, 76));
                 // echo "<br>";
--- index.php
+++ index.php
@@ -13,10 +13,27 @@
 </head>
 <body>
     <form >
-        <label for="" name="length" >Lunghezza Password:</label>
+        <label for="length">Lunghezza Password:</label>
         <input type="text" name="length">
+        <br>
+        <span>Consenti ripetizioni di uno o più caratteri: </span>
+        <input type="radio" name="repetition_si" value="Si" id="repetitionChoice1">
+        <label for="repetitionChoice1">si</label>
+        <input type="radio" name="repetition_no" value="No" id="repetitionChoice2">
+        <label for="repetitionChoice2">no</label>
+        <br>
+        <input type="text" name="numbers">
+        <label for="numbers">Numeri</label>
+        <br>
+        <input type="text" name="letters">
+        <label for="letters">Lettere</label>
+        <br>
+        <input type="text" name="symbols">
+        <label for="symbols">Simboli</label>
+        <br>
         <input type="submit">
 
+    
         <!-- <span>La tua password è:  -->
             <?php  
                 session_start();
